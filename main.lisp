2ef7e9910f8fa258002978a26c750d25f481cf6e
Schell Scivally <efsubenovex@gmail.com>
2010-07-03 11:55:10 -0700

    has main

---
+++ main.lisp
@@ -0,0 +1,13 @@
+#!
+; Bourtange - a fort defense game
+; Schell Scivally
+; Aaron Maus 
+; Sat Jul  3 11:31:06 PDT 2010
+; 
+; things we need - asdf, asdf-install, cffi, babel, alexandria, trivial-features
+
+(defun pl (string)
+	(format t string)
+)
+
+(pl "Bourtange")
